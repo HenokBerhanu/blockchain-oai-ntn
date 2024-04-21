5783a5495c66c9d04139dca63e7ad08e8abaa48e
henok berhanu tsegaye <henokberhanut@gmail.com>
2024-04-21 16:44:56 -0600

    updates

---
+++ build-images/oai-core/buil_core.sh
@@ -0,0 +1,12 @@
+
+# from the directory: /home/henok/blockchain-oai-ntn/oai-cn5g-fed/build-images/oai-core/component
+
+sudo docker build --tag oai-upf:v1.1.0 --file docker/Dockerfile.upf.ubuntu --build-arg BASE_IMAGE=ubuntu:jammy .
+
+sudo docker build --tag oai-smf:v2.0.1 --file docker/Dockerfile.smf.ubuntu --build-arg BASE_IMAGE=ubuntu:jammy .
+
+sudo docker build --tag oai-udr:v2.0.1 --file docker/Dockerfile.udr.ubuntu --build-arg BASE_IMAGE=ubuntu:jammy .
+
+sudo docker build --tag oai-udm:v2.0.1 --file docker/Dockerfile.udm.ubuntu --build-arg BASE_IMAGE=ubuntu:jammy .
+
+sudo docker build --tag oai-ausf:v2.0.1 --file docker/Dockerfile.ausf.ubuntu --build-arg BASE_IMAGE=ubuntu:jammy .
